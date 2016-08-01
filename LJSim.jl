8051e86c600cc04b195c3397bee42d6529a72f29
abieler <andre.bieler81@gmail.com>
2016-08-01 16:05:37 +0200

    column major

--- LJSim.jl
+++ LJSim.jl
@@ -5,9 +5,9 @@ function main()
     boxl = 22.5
     boxl2 = boxl/2.0
     coords = readtable("configuration.dat", separator=' ', header=false)
-    coords = convert(Matrix{Float64}, coords)
+    coords = convert(Matrix{Float64}, coords)'
     println(size(coords))
-    npart = size(coords, 1)
+    npart = size(coords, 2)
     temp = 1.8
     beta = 1.0 / temp
     println(npart)
@@ -18,7 +18,7 @@ function main()
     traj = open("Traj_jl.xyz","w")
     write(traj, string(npart) * "\n\n\n")
     for i in 1:npart
-      @printf traj "Ar %.3f %.3f %.3f\n" coords[i,1] coords[i,2] coords[i,3]
+      @printf traj "Ar %.3f %.3f %.3f\n" coords[1,i] coords[2,i] coords[3,i]
     end
 
     println("Initial Energy: ", E_T)
@@ -31,42 +31,42 @@ function main()
 
     finalconfig = open("final_configuration_jl.dat", "w")
     for i in 1:npart
-      @printf finalconfig "%.3f %.3f %.3f" coords[i,1] coords[i,2] coords[i,3]
+      @printf finalconfig "%.3f %.3f %.3f" coords[1,i] coords[2,i] coords[3,i]
     end
 
     write(traj, npart, "\n\n")
     for i in 1:npart
-      @printf traj "Ar %.3f %.3f %.3f\n" coords[i,1] coords[i,2] coords[i,3]
+      @printf traj "Ar %.3f %.3f %.3f\n" coords[1,i] coords[2,i] coords[3,i]
     end
     close(finalconfig)
     close(traj)
 end
 
 function montecarlo_loop(ncycle, coords, beta, E_T, boxl, boxl2)
-    npart = size(coords, 1)-1
+    npart = size(coords, 2) - 1
     disp = zeros(Float64, 3)
     e_diff = 0.0
     nmove = 0
-    for indx in 1:ncycle+1
+    for indx in 1:ncycle + 1
         nmove = 1 + floor(Int, npart * rand())
         for i in 1:3
           disp[i] = 0.2 * (2.0 * rand()-1.0)
-          if abs(disp[i] + coords[nmove,i]) > boxl
-            disp[i] = disp[i] - copysign(boxl,disp[i])
+          if abs(disp[i] + coords[i, nmove]) > boxl
+            disp[i] = disp[i] - copysign(boxl, disp[i])
           end
         end
         e_diff = shift_ecalc(npart, coords, nmove, disp, boxl, boxl2)
         if e_diff < 0.0
           E_T += e_diff
-          coords[nmove,1] += disp[1]
-          coords[nmove,2] += disp[2]
-          coords[nmove,3] += disp[3]
+          for k in 1:3
+	    coords[k,nmove] += disp[k]
+          end
         else
           if exp(-beta*e_diff) > rand()
             E_T += e_diff
-            coords[nmove,1] += disp[1]
-            coords[nmove,2] += disp[2]
-            coords[nmove,3] += disp[3]
+            for k in 1:3
+              coords[k,nmove] += disp[k]
+            end
           end
         end
     end
@@ -82,9 +82,9 @@ function detailed_ecalc(npart, coords, boxl, boxl2)
   energy = 0.0
   for i in 1:npart-1
     for j in i+1:npart
-      rx = coords[i, 1] - coords[j, 1]
-      ry = coords[i, 2] - coords[j, 2]
-      rz = coords[i, 3] - coords[j, 3]
+      rx = coords[1,i] - coords[1,j]
+      ry = coords[2,i] - coords[2,j]
+      rz = coords[3,i] - coords[3,j]
       if abs(rx) > boxl2
         rx -= copysign(boxl, rx)
       end
@@ -111,9 +111,9 @@ function shift_ecalc(npart,coords,nmove,disp, boxl, boxl2)
   energy = 0.0
   for j in 1:npart
     if j != nmove
-      @inbounds rx = coords[nmove,1] + disp[1] - coords[j,1]
-      @inbounds ry = coords[nmove,2] + disp[2] - coords[j,2]
-      @inbounds rz = coords[nmove,3] + disp[3] - coords[j,3]
+      @inbounds rx = coords[1,nmove] + disp[1] - coords[1,j]
+      @inbounds ry = coords[2,nmove] + disp[2] - coords[2,j]
+      @inbounds rz = coords[3,nmove] + disp[3] - coords[3,j]
       if abs(rx) > boxl2
         rx -= copysign(boxl, rx)
       end
@@ -132,9 +132,9 @@ function shift_ecalc(npart,coords,nmove,disp, boxl, boxl2)
           energy = energy + LJ
       end
 
-      @inbounds rx = coords[nmove,1] - coords[j,1]
-      @inbounds ry = coords[nmove,2] - coords[j,2]
-      @inbounds rz = coords[nmove,3] - coords[j,3]
+      @inbounds rx = coords[1,nmove] - coords[1,j]
+      @inbounds ry = coords[2,nmove] - coords[2,j]
+      @inbounds rz = coords[3,nmove] - coords[3,j]
       if abs(rx) > boxl2
           rx -= copysign(boxl,rx)
       end
